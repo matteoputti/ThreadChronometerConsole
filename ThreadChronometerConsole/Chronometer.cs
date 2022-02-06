4453670d7f6020a184da860466c02475a689baa9
Matteo <Matteo@DESKTOP-LR8CVPU>
2022-02-06 17:22:26 +0100

    Add project files.

---
+++ ThreadChronometerConsole/Chronometer.cs
@@ -0,0 +1,38 @@
+﻿namespace ThreadChronometerConsole
+{
+    public class Chronometer
+    {
+        private int secondsElapsed;
+        private static Mutex mutex = new Mutex();
+
+        public Chronometer()
+        {
+            secondsElapsed = 0;
+        }
+
+        public void calculateElapsedSeconds()
+        {
+            while (true)
+            {
+                if (mutex.WaitOne())
+                {
+                    secondsElapsed++;
+                    Thread.Sleep(1000);
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+
+        public void printElapsedSeconds()
+        {
+            while (true)
+            {
+                if (mutex.WaitOne())
+                {
+                    Console.WriteLine($"Seconds elapsed: {secondsElapsed}");
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+    }
+}
---
+++ ThreadChronometerConsole/Main.cs
@@ -0,0 +1,7 @@
+﻿using ThreadChronometerConsole;
+
+Chronometer chrono = new Chronometer();
+Thread t1 = new Thread(new ThreadStart(chrono.calculateElapsedSeconds));
+Thread t2 = new Thread(new ThreadStart (chrono.printElapsedSeconds));
+t1.Start();
+t2.Start();
